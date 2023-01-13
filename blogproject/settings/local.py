17e599abb3e0ca7802aa0bc6c5986a82a17c0765
clay <1093591428@qq.com>
2023-01-14 04:04:34 +0800

    first commit5

---
+++ blogproject/settings/local.py
@@ -0,0 +1,13 @@
+# --*--coding: utf-8 --*--
+# @Time: 2023-01-14 3:50
+# @Author: 李月初
+# @FIle: local
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'development-secret-key'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
