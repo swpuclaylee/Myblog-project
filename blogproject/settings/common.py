a03a7c9345b94c35feb6b28c1bf4817883654f9b
clay <1093591428@qq.com>
2023-01-19 07:05:46 +0800

    first commit12

--- blogproject/settings/common.py
+++ blogproject/settings/common.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'media'),
+)
 
 
 # django-pure-pagination 分页设置
