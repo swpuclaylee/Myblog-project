533948cdf8a5a61060c0b507d277aae90bd704fe
clay <1093591428@qq.com>
2023-01-24 15:36:13 +0800

    first commit12

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -9,10 +9,10 @@ from .common import *
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '101.132.185.146', '.claylwz.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '101.132.185.146', 'claylwz.com']
 
 HAYSTACK_CONNECTIONS['default']['URL'] = 'http://101.132.185.146:9200/'
 
