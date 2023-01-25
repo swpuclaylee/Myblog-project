c3aaf0c4f7878724ebbf8dffca96b3ee2eadbdd6
clay <1093591428@qq.com>
2023-01-25 15:18:56 +0800

    first commit12

--- comments/models.py
+++ comments/models.py
@@ -8,7 +8,8 @@ class Comment(models.Model):
     name = models.CharField('名字', max_length=50)
     email = models.EmailField('邮箱')
     url = models.URLField('网址', blank=True)
-    text = RichTextUploadingField('内容')
+    #text = RichTextUploadingField('内容')
+    text = models.TextField('内容')
     created_time = models.DateTimeField('创建时间', default=timezone.now)
     post = models.ForeignKey('blog.Post', verbose_name='文章', on_delete=models.CASCADE)
 
