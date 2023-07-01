a3807464e8efe5e3c041004c4d922113f7c8c550
clay <1093591428@qq.com>
2023-07-02 00:02:20 +0800

    first commit12

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -9,7 +9,7 @@ from .common import *
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.claylwz.com', '139.9.129.148']
