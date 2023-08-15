eaa0b2ccab2621d8d8c806423e82b58949d2dd69
clay <1093591428@qq.com>
2023-08-15 10:37:08 +0800

    异步短信通知

--- celery_tasks/celery.py
+++ celery_tasks/celery.py
@@ -6,8 +6,15 @@ from celery import Celery
 from django.conf import settings
 
 import os
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Myblog-project.blogproject.settings.production')
+# 将项目根目录添加到 Python 的模块搜索路径中
+back = os.path.dirname
+BASE_DIR = back(back(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogproject.settings.production')
 
 app = Celery('celery_tasks')
 app.config_from_object("celery_tasks.config")
