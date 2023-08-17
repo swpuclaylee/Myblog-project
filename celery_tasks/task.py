559ba15888c70aaabc7b33a30bb9530692876830
clay <1093591428@qq.com>
2023-08-18 01:26:03 +0800

    异步短信通知

--- celery_tasks/task.py
+++ celery_tasks/task.py
@@ -11,13 +11,13 @@ import pickle
 @app.task
 def send_mail_task(name, flag):
     name = pickle.loads(name)
-    d_name = name.encode('utf-8').decode('unicode_escape')
+    #d_name = name.encode('utf-8').decode('unicode_escape')
     if flag:
         subject = "you have a comment"
-        message = f"you received a comment from{d_name}"
+        message = f"you received a comment from{name}"
     else:
         subject = 'someone to contact'
-        message = f'you received a contact from{d_name}'
+        message = f'you received a contact from{name}'
     from_email = '1093591428@qq.com'
     recipients = ['swlz4751@gmail.com']
     send_mail(subject, message, from_email, recipients)
