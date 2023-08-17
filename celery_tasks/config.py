90f8ac08053c8a6a4a7efce007f72769d5b73869
clay <1093591428@qq.com>
2023-08-18 00:02:11 +0800

    异步短信通知

--- celery_tasks/config.py
+++ celery_tasks/config.py
@@ -5,7 +5,7 @@
 
 CELERY_BROKER_URL = 'amqp://guest@localhost//'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Shanghai'
