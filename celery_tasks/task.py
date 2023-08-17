1b2b2ab0fd453bf067e8938b1cf80111a185f8c7
clay <1093591428@qq.com>
2023-08-18 00:43:54 +0800

    异步短信通知

--- celery_tasks/task.py
+++ celery_tasks/task.py
@@ -11,13 +11,13 @@ import json
 @app.task
 def send_mail_task(name, flag):
     name = json.loads(name)
-    name = name.encode('utf-8').decode('unicode_escape')
+    #name = name.encode('utf-8').decode('unicode_escape')
     if flag:
-        subject = "you have a comment"
-        message = "you received a comment from{}".format(name)
+        subject = u"you have a comment"
+        message = u"you received a comment from{}".format(name)
     else:
-        subject = 'someone to contact'
-        message = 'you received a contact from{}'.format(name)
+        subject = u'someone to contact'
+        message = u'you received a contact from{}'.format(name)
     from_email = '1093591428@qq.com'
     recipients = ['swlz4751@gmail.com']
     send_mail(subject, message, from_email, recipients)
