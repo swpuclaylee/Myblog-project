63b3760b29871d4e8328d2e5e1ca4a542fd970fd
clay <1093591428@qq.com>
2023-08-17 22:27:23 +0800

    异步短信通知

--- celery_tasks/task.py
+++ celery_tasks/task.py
@@ -9,11 +9,11 @@ from django.core.mail import send_mail
 @app.task
 def send_mail_task(name, flag):
     if flag:
-        subject = "you have a comment"
-        message = "you received a comment"
+        subject = "you have a comment".encode('utf-8')
+        message = "you received a comment from{}".format(name).encode('utf-8')
     else:
-        subject = 'someone to contact'
-        message = 'you received a contact'
+        subject = 'someone to contact'.encode('utf-8')
+        message = 'you received a contact from{}'.format(name).encode('utf-8')
     from_email = '1093591428@qq.com'
     recipients = ['swlz4751@gmail.com']
     send_mail(subject, message, from_email, recipients)
