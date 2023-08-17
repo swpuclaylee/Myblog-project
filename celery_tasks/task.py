6aa3825defa34abf8229886197b95300c7b159b2
clay <1093591428@qq.com>
2023-08-17 22:01:51 +0800

    异步短信通知

--- celery_tasks/task.py
+++ celery_tasks/task.py
@@ -9,11 +9,11 @@ from django.core.mail import send_mail
 @app.task
 def send_mail_task(name, flag):
     if flag:
-        subject = "你有一条评论"
-        message = f"你收到了一条来自{name}的评论"
+        subject = "you have a comment"
+        message = "you received a comment"
     else:
-        subject = '有人联系你了！'
-        message = f'你收到了一条来自{name}的联系'
+        subject = 'someone to contact'
+        message = 'you received a contact'
     from_email = '1093591428@qq.com'
     recipients = ['swlz4751@gmail.com']
     send_mail(subject, message, from_email, recipients)
