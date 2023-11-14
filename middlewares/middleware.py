d0488d026b0c6bb84042a85753da8375e21e75f3
clay <1093591428@qq.com>
2023-11-14 23:55:44 +0800

    浏览量设置

--- middlewares/middleware.py
+++ middlewares/middleware.py
@@ -14,13 +14,25 @@ class GlobalViewCountMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
         if not request.path.startswith('/xadmin/') and response.status_code == 200 and request.method == "GET":  # 排除管理员后台的请求
-            # 递增全局浏览量
-            GLOBAL_VIEW_COUNT = cache.get('global_view_count', 0)
+            site_view_obj = SiteView.objects.first()
+            if site_view_obj:
+                GLOBAL_VIEW_COUNT = site_view_obj.site_view_count
+            else:
+                GLOBAL_VIEW_COUNT = cache.get('global_view_count', 0)
             cache.set('global_view_count', GLOBAL_VIEW_COUNT + 1)
-            site_view = SiteView.objects.first()  # 假设 SiteView 模型只有一个对象
-            if not site_view:
+            if not site_view_obj:
                 SiteView.objects.create(site_view_count=GLOBAL_VIEW_COUNT)
             else:
-                site_view.site_view_count = GLOBAL_VIEW_COUNT
-                site_view.save()
+                site_view_obj.site_view_count = GLOBAL_VIEW_COUNT
+                site_view_obj.save()
+
+            # # 递增全局浏览量
+            # GLOBAL_VIEW_COUNT = cache.get('global_view_count', 0)
+            # cache.set('global_view_count', GLOBAL_VIEW_COUNT + 1)
+            # site_view = SiteView.objects.first()  # 假设 SiteView 模型只有一个对象
+            # if not site_view:
+            #     SiteView.objects.create(site_view_count=GLOBAL_VIEW_COUNT)
+            # else:
+            #     site_view.site_view_count = GLOBAL_VIEW_COUNT
+            #     site_view.save()
         return response
