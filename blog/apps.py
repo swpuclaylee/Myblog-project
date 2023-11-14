b48e84634f532b2e5a8cc3e57cf271018019acf5
clay <1093591428@qq.com>
2023-11-14 23:49:39 +0800

    浏览量设置

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
-from django.core.cache import cache
-from .models import SiteView
 
 
 class BlogConfig(AppConfig):
@@ -9,8 +7,4 @@ class BlogConfig(AppConfig):
 
     def ready(self):
         import blog.signals
-
-        site_view_obj = SiteView.objects.first()
-        if site_view_obj:
-            cache.set('global_view_count', site_view_obj.site_view_count)
 
